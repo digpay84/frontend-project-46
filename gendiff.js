90b0504b3c33ccca2f553831fd8339437852b478
Dmitry Levchenko <lds84pro@gmail.com>
2026-04-02 00:07:10 +0300

    fix

--- gendiff.js
+++ gendiff.js
@@ -12,9 +12,14 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0', '-V, --version', 'output the version number')
-  .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format (stylish, plain, json)', 'stylish')
+  .argument('[filepath1]', 'first file path')
+  .argument('[filepath2]', 'second file path')
   .action((filepath1, filepath2, options) => {
+    if (!filepath1 || !filepath2) {
+      program.error('missing required argument \'filepath1\'');
+    }
+
     const file1 = fs.readFileSync(filepath1, 'utf8');
     const file2 = fs.readFileSync(filepath2, 'utf8');
 
